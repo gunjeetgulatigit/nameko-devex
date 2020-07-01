3feecb53308ec2ac193047521880ea9f79373d71
Coder Ricko <coddericko>
2020-06-30 20:31:56 -0700

    smoketest

--- devops/nex-smoketest.sh
+++ devops/nex-smoketest.sh
@@ -36,7 +36,7 @@ echo STD_APP_URL=${STD_APP_URL}
 
 # Test: Create Products
 echo "=== Creating a product id: the_odyssey ==="
-curl -XPOST  "${STD_APP_URL}/products" \
+curl -s -XPOST  "${STD_APP_URL}/products" \
 	 -d '{"id": "the_odyssey", "title": "The Odyssey", "passenger_capacity": 101, "maximum_speed": 5, "in_stock": 10}'
 echo
 # Test: Get Product
@@ -47,7 +47,7 @@ curl -s "${STD_APP_URL}/products/the_odyssey" | jq .
 echo "=== Creating Order ==="
 ORDER_ID=$(curl -s -XPOST -d '{"order_details": [{"product_id": "the_odyssey", "price": "100000.99", "quantity": 1}]}' "${STD_APP_URL}/orders")
 echo ${ORDER_ID}
-ID=$(echo ${ORDER_ID} | jq '.id')
+ID=$(echo ${ORDER_ID} | jq -r '.id')
 
 # Test: Get Order back
 echo "=== Getting Order ==="
